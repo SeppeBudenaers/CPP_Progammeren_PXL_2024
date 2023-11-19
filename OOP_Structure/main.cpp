c3cbb659506f0dc589e650e17d10dae8573cb648
seppe budenaers <101107875+SeppeBudenaers@users.noreply.github.com>
2023-11-19 20:40:42 +0100

    tested something

--- OOP_Structure/main.cpp
+++ OOP_Structure/main.cpp
@@ -6,13 +6,15 @@ using namespace std;
 
 int main()
 {
-    Pawn pion1(1,0);
+    Pawn * foo;
+    foo = new Pawn(1,0);
     Pawn pion2(1,1);
     King king (0,3);
-    cout <<"("<<pion1.Xcordinate<< "," <<pion1.Ycordinate<< ") " <<pion1.Move()<< " end cordinate ("<<pion1.Xcordinate<< "," <<pion1.Ycordinate<< ") " << endl;
-    cout <<"("<<pion1.Xcordinate<< "," <<pion1.Ycordinate<< ") " <<pion1.Move()<< " end cordinate ("<<pion1.Xcordinate<< "," <<pion1.Ycordinate<< ") " << endl;
+    cout <<"("<<foo->Xcordinate<< "," <<foo->Ycordinate<< ") " <<foo->Move()<< " end cordinate ("<<foo->Xcordinate<< "," <<foo->Ycordinate<< ") " << endl;
+    //cout <<"("<<pion1.Xcordinate<< "," <<pion1.Ycordinate<< ") " <<pion1.Move()<< " end cordinate ("<<pion1.Xcordinate<< "," <<pion1.Ycordinate<< ") " << endl;
     cout <<"("<<pion2.Xcordinate<< " " <<pion2.Ycordinate<< ") " <<pion2.Move()<< " end cordinate ("<<pion2.Xcordinate<< "," <<pion2.Ycordinate<< ") " << endl;
     cout <<"("<<king.Xcordinate<< "," <<king.Ycordinate<< ") " <<king.Move()<< " end cordinate ("<<king.Xcordinate<< "," <<king.Ycordinate<< ") " << endl;
     cout <<"("<<pion2.Xcordinate<< "," <<pion2.Ycordinate<< ") " <<pion2.Move()<< " end cordinate ("<<pion2.Xcordinate<< "," <<pion2.Ycordinate<< ") " << endl;
+    delete foo;
     return 0;
 }
